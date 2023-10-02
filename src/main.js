c270ef54b5998ddf5a02231cd9b5df3fa5dfe5e2
IIROFESSOR-X <IIROFESSOR@gmail.com>
2023-10-02 23:04:09 +0300

    mark 7

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ const closeBtn = document.querySelector(".menu-close-btn");
 
 // когда доделаете хедер раскомментируйте эту часть кода
 
-// openBtn.addEventListener("click", function () {
-//     menu.classList.toggle("is-open");
-//     document.body.style.overflow = "hidden";
-// });
+openBtn.addEventListener("click", function () {
+    menu.classList.toggle("is-open");
+    document.body.style.overflow = "hidden";
+});
 
 closeBtn.addEventListener("click", function () {
     menu.classList.toggle("is-open");
